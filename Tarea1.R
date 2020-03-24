c40fa2587eeea85eec8729fa358e6e2037ab4cfd
hhorgz <hanns.guzman@gmail.com>
2020-03-23 20:34:33 -0600

    Finalizacion de tarea 1

--- Tarea1.R
+++ Tarea1.R
@@ -64,3 +64,76 @@ median(subset(datos_migracion$PEI4, datos_migracion$PEI3 == "Mujer"))
 #                       VIVIENDA_BDP
 ########################################################################
 datosVivienda <- read.spss("VIVIENDA_BDP.sav", to.data.frame = T)
+
+# INCISO 8
+# Determinar el tipo de vivienda y el material de construccion en el municipio de Santa Apolonia, Chimaltenango
+View(datosVivienda)
+viviendaChimaltenango <- subset(datosVivienda, datosVivienda$DEPARTAMENTO == "Chimaltenango" & datosVivienda$MUNICIPIO == "Santa Apolonia")
+class(viviendaChimaltenango)
+table(
+  viviendaChimaltenango$PCV1, viviendaChimaltenango$PCV2
+)
+write.csv(
+  table(viviendaChimaltenango$PCV1, viviendaChimaltenango$PCV2),
+  "TipoVivienda_vs_Material.csv"
+)
+
+########################################################################
+#                       HOGAR_BDP.sav
+########################################################################
+datosHogar <- read.spss("HOGAR_BDP.sav", to.data.frame = TRUE)
+# INCISO 9
+# Determine la proporcion de la variable computadora vrs internet a nivel nacional
+View(datosHogar)
+# computadora   datosHogar$PCH9_H
+# internet      datosHogar$PCH9_I
+
+computadoraVsInternet <- table(datosHogar$PCH9_H, datosHogar$PCH9_I)
+computadoraVsInternet
+rownames(computadoraVsInternet) = c("Con Computadora", "Sin Computadora")
+colnames(computadoraVsInternet) = c("Con Internet", "Sin Internet")
+round(100 * computadoraVsInternet / sum(computadoraVsInternet), 1)
+
+# INCISO 10
+# Realice un grafico de Pie q muestre la proporcion de la principal fuente para cocinar
+fuenteCocinar <- table(datosHogar$PCH14)
+fuenteCocinar
+etiquetasPorcentaje <- round(100 * fuenteCocinar / sum(fuenteCocinar), 2)
+etiquetasPorcentaje
+rownames(fuenteCocinar)
+pie(
+  etiquetasPorcentaje,
+  labels = paste(etiquetasPorcentaje, "%"),
+  main = "Principal fuente para cocinar a nivel nacional",
+  radius = 1,
+  cex = 0.8,
+  col = rainbow(length(rownames(etiquetasPorcentaje)))
+  )
+legend(
+  "topright", 
+  rownames(etiquetasPorcentaje),
+  fill = rainbow(length(rownames(etiquetasPorcentaje))),
+  cex = 0.8
+)
+
+########################################################################
+#                       PERSONA_BDP.sav
+########################################################################
+datosPersona <- read.spss("PERSONA_BDP.sav", to.data.frame = TRUE)
+View(datosPersona)
+
+# INCISO 11
+# Determine el porcentaje de hombres y mujeres que usaron el telefono en los ultimos 3 meses por area rural y urbana
+
+# ha utilizado celular en los ultimos tres meses = PCP26_A
+# sexo de la persona = PCP6
+# area = area
+utilizacionTelefono <- table(datosPersona$PCP6, datosPersona$AREA, datosPersona$PCP26_A)
+utilizacionTelefono[,,1]
+
+########################################################################
+#                       Guatemala.txt
+########################################################################
+
+# INCISO 12
+# Usando el archivo Guatemala.txt, determine los estadisticos por mes para cada una de las estaciones
